092cb2d11132235a698860ae44b0f9e6b91c8469
Shishir Bhat <shishirvbhat@gmail.com>
2019-01-03 00:31:10 -0500

    RegisterSet only deals with Shorts

--- src/main/scala/com/sleet/runtime/RegisterSet.scala
+++ src/main/scala/com/sleet/runtime/RegisterSet.scala
@@ -1,26 +1,15 @@
 package com.sleet.runtime
 
-object RegisterSet {
-  val MinimumRegisterValue: Int = -Math.pow(2, 15).toInt
-  val MaximumRegisterValue: Int = Math.pow(2, 15).toInt - 1
-}
+case class RegisterSet(registers: Vector[Short]) {
+  require(registers.size == 7)
 
-class RegisterSet(initialVals: Vector[Int]) {
-  require(initialVals.size == 7)
-  require(initialVals.forall(validate))
-
-  private def validate(value: Int): Boolean =
-    RegisterSet.MinimumRegisterValue <= value && value <= RegisterSet.MaximumRegisterValue
-
-  private val registers: Vector[Int] = initialVals
-
-  def apply(register: Int): Int = {
+  def apply(register: Int): Short = {
     require(register >= 0 && register <= 7)
     registers(register)
   }
 
-  def updated(register: Int, value: Int): RegisterSet = {
+  def updated(register: Int, value: Short): RegisterSet = {
     require(register >= 0 && register <= 7)
-    new RegisterSet(registers.updated(register, value))
+    RegisterSet(registers.updated(register, value))
   }
 }
