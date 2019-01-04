a968fb6f3c18eade87cae71c663726b9ac85be6c
Shishir Bhat <shishirvbhat@gmail.com>
2019-01-04 01:20:14 -0500

    Layout more of the Assembler API

--- src/main/scala/com/sleet/assembler/Assembler.scala
+++ src/main/scala/com/sleet/assembler/Assembler.scala
@@ -1,11 +1,38 @@
 package com.sleet.assembler
 
 import com.sleet.runtime.{Instruction, State}
+import scala.language.implicitConversions
 
 object Assembler {
-  def assemble(source: Traversable[String]): State = ???
+
+  private type SymbolTable = Map[String, Int]
+
+  def assemble(source: Iterable[String]): State = {
+    val lines = source.view
+      .map(stripComments)
+      .map(_.trim.toUpperCase)
+      .filterNot(_.isEmpty).force
+    ???
+  }
 
   def toInstruction(line: Short): Instruction = ???
 
-  def toInstruction(line: String): Instruction = ???
+  def toInstruction(line: String, label: Option[String]): Option[Instruction] =
+    LC3InstructionPatterns.toInstruction(line, label)
+
+  private def buildSymbolTable(source: Iterable[String]): SymbolTable = {
+    implicit def int2Short(x: Int): Short = x.toShort
+
+    def buildTable(currentPC: Option[Short], table: SymbolTable, lines: Iterable[String]): SymbolTable = {
+      if (lines.isEmpty) table
+      else {
+        val currLine = lines.head
+
+      }
+    }
+
+    buildTable(None, Map(), source)
+  }
+
+  private def stripComments(line: String): String = line.takeWhile(_ != ';')
 }
