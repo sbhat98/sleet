d1ce86fde9f5d0c57847622796608947017c7fdd
Shishir Bhat <shishirvbhat@gmail.com>
2019-01-07 21:07:59 -0500

    USe breaks for symbol table creation

--- src/main/scala/com/sleet/assembler/Assembler.scala
+++ src/main/scala/com/sleet/assembler/Assembler.scala
@@ -2,16 +2,18 @@ package com.sleet.assembler
 
 import com.sleet.runtime.{Instruction, State}
 import scala.language.implicitConversions
+import util.control.Breaks._
 
 object Assembler {
 
   private type SymbolTable = Map[String, Int]
 
-  def assemble(source: Iterable[String]): State = {
+  def assemble(source: Seq[String]): State = {
     val lines = source.view
       .map(stripComments)
       .map(_.trim.toUpperCase)
-      .filterNot(_.isEmpty).force
+      .zip(source.indices.map(_ + 1))
+      .filter{case (line, _) => line.nonEmpty}.force
     ???
   }
 
@@ -21,8 +23,31 @@ object Assembler {
     LC3InstructionPatterns.toInstruction(line, label)
 
   // Returns the constructed symbol table and the code stripped of prefix labels
-  private def buildSymbolTable(source: Iterable[String]): (SymbolTable, Iterable[String]) = {
-    implicit def int2Short(x: Int): Short = x.toShort
+  private def buildSymbolTable(source: Seq[(String, Int)]): (SymbolTable, Seq[String]) = {
+    var table: SymbolTable = Map()
+    var strippedLines: List[(String, Int)] = Nil
+    var currentPc: Option[Int] = None
+
+    val orig = raw"\.ORIG\s*(X\d{4}|-?\d+)".r
+
+    for ((line, lineNum) <- source) {
+      breakable {
+        if (currentPc.isEmpty) {
+          line match {
+            case orig(num) =>
+              currentPc = Some(numStr2Short(num, 16))
+            case _ => throw new SyntaxError("Expected a .orig directive on line" + lineNum)
+          }
+          break
+        } else if (line.matches(raw"\.END")) {
+          currentPc = None
+          break
+        }
+        val currPc = currentPc.get
+        if (line.matches())
+      }
+    }
+
     ???
   }
 
